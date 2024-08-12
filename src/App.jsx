6d6cd802c1336638eb623ca0bdbf391080892a91
Soyagvs <agustinmartinezyt@gmail.com>
2024-08-11 21:52:46 -0300

    add json

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home'
 import { ViewImages } from './pages/ViewImages';
 import { ScannerQr } from './pages/ScannerQr';
-import { ViewMobile } from './pages/ViewMobile';
+import { ViewMobile } from './pages/ViewMobile';  
 
 function App() {
 
