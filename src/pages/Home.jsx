84fcbba8abe35969793985ba57b55ecc3e3c9c86
Soyagvs <agustinmartinezyt@gmail.com>
2024-08-11 21:53:52 -0300

    add target

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
                 <h1 className="text-8xl text-slate-200 font-semibold">Bienvenidos a <mark className="rounded-md">Evento Foto</mark></h1>
                 <p className="text-2xl text-slate-500 font-semibold">En este sitio podras compartir imagenes en tiempo real o de tu galeria.</p>
                 <div className="flex justify-center items-center gap-3">
-                    <Link className="bg-orange-300 h-14 w-44 flex justify-center items-center rounded-md text-lg text-black" to="/view-images" >Ver las imagenes</Link>
+                    <Link className="bg-orange-300 h-14 w-44 flex justify-center items-center rounded-md text-lg text-black" to="/view-images" target='_blank'>Ver las imagenes</Link>
                     <Link className="bg-blue-300 h-14 w-36 flex justify-center items-center rounded-md text-lg text-black" to="/scanner-qr" target='_blank' >Escanear QR</Link>
                 </div>
             </section>
