9906f73c21925ee9f1e0512f77d4ac7d66188be1
soyagvs <agustinmartinezyt@gmail.com>
2024-08-12 22:56:32 -0300

    new title

--- src/pages/ViewImages.jsx
+++ src/pages/ViewImages.jsx
@@ -37,8 +37,8 @@ export const ViewImages = () => {
 
     return (
         <main className="flex flex-col justify-center items-center min-h-screen bg-slate-900">
-            <h2 className="text-white text-2xl text-center">Aquí se visualizará la imagen más reciente</h2>
-            <div className="max-sm:w-[350px] max-sm:h-[350px] sm:w-[400px] sm:h-[400px] md:w-[700px] md:h-[700px] lg:w-[900px] lg:h-[800px] bg-slate-900 rounded-lg mt-5 flex justify-center items-center object-cover">
+            <h2 className="text-white text-2xl text-center mt-5">Aquí se visualizará la imagen más reciente</h2>
+            <div className=" w-auto h-auto bg-slate-900 rounded-lg mt-5 flex justify-center items-center object-cover">
                 {error ? (
                     <p className="text-red-500">{error}</p>
                 ) : imageUrl ? (
